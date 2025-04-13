dbe61ab479d105bf2e1cd7797eb234b6890ec63c
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-04-13 23:32:39 +0700

    feat: enhance AboutSection with animated ToolboxItems and improved background effects

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -112,11 +112,15 @@ export const AboutSection = () => {
                 digital experiences."
                 className=""
               />
-              <ToolboxItems items={toolboxItems} className="" />
+              <ToolboxItems
+                items={toolboxItems}
+                className=""
+                itemsWrapperClassname="animate-move-left [animation-duration:30s]"
+              />
               <ToolboxItems
                 items={toolboxItems}
                 className="mt-6"
-                itemsWrapperClassname="-translate-x-1/2"
+                itemsWrapperClassname="animate-move-right [animation-duration:15s]"
               />
             </Card>
           </div>
@@ -151,7 +155,9 @@ export const AboutSection = () => {
                 className="h-full w-full object-cover object-left-top"
                 alt="map"
               />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]" />
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10" />
                 <Image
                   src={SmileImage}
                   className="size-20"
