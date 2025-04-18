691d4217877e4d410e1499d342f672e7389ba552
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-04-19 06:47:04 +0700

    feat: add multiple experience images and index file for improved asset management

---
+++ src/assets/experiences/index.ts
@@ -0,0 +1,75 @@
+import SmartcoopAcademyIndex from "./SmartcoopAcademyIndex.png";
+import SmartcoopAcademyQuiz from "./SmartcoopAcademyQuiz.png";
+import SmartcoopAcademyRegister from "./SmartcoopAcademyRegister.png";
+import SmartcoopAcademyTrainingModule from "./SmartcoopAcademyTrainingModule.png";
+import BprChristaJayaDashboard from "./BprChristaJayaDashboard.png";
+import BprChristaJayaIndex from "./BprChristaJayaIndex.png";
+import BprChristaJayaInformation from "./BprChristaJayaInformation.png";
+import BprChristaJayaLogin from "./BprChristaJayaLogin.png";
+import DiabseninAnnouncement from "./DiabseninAnnouncement.png";
+import DiabseninAnnouncementDetail from "./DiabseninAnnouncementDetail.png";
+import DiabseninLogin from "./DiabseninLogin.png";
+import DiabseninMaps from "./DiabseninMaps.png";
+import DiabseninNotification from "./DiabseninNotification.png";
+import DiabseninSignin from "./DiabseninSignin.png";
+import BigDataUMKMCatalog from "./BigDataUMKMCatalog.png";
+import BigDataUMKMIndex from "./BigDataUMKMIndex.png";
+import BigDataUMKMDashboard from "./BigDataUMKMDashboard.png";
+import BigDataUMKMLogin from "./BigDataUMKMLogin.png";
+import PortalSmartcoopCoperationData from "./PortalSmartcoopCoperationData.png";
+import PortalSmartcoopDashboardStatistik from "./PortalSmartcoopDashboardStatistik.png";
+import PortalSmartcoopLogin from "./PortalSmartcoopLogin.png";
+import PortalSmartcoopStatistik from "./PortalSmartcoopStatistik.png";
+import SekiHalselDashboard from "./SekiHalselDashboard.png";
+import SekiHalselLogin from "./SekiHalselLogin.png";
+import SilatoKopiSahaDashboard from "./SilatoKopiSahaDashboard.png";
+import SilatoKopiSahaIndex from "./SilatoKopiSahaIndex.png";
+import SilatoKopiSahaLogin from "./SilatoKopiSahaLogin.png";
+import SilatoKopiSahaTraining from "./SilatoKopiSahaTraining.png";
+import NeqatAnnouncement from "./NeqatAnnouncement.png";
+import NeqatCheckin from "./NeqatCheckin.png";
+import NeqatConfig from "./NeqatConfig.png";
+import NeqatMaps from "./NeqatMaps.png";
+import SipajongaPayment from "./SipajongaPayment.png";
+import SipajongaStatistik from "./SipajongaStatistik.png";
+import PmsDetailTask from "./PmsDetailTask.png";
+import PmsTask from "./PmsTask.png";
+
+export {
+  SmartcoopAcademyIndex,
+  SmartcoopAcademyQuiz,
+  SmartcoopAcademyRegister,
+  SmartcoopAcademyTrainingModule,
+  BprChristaJayaDashboard,
+  BprChristaJayaIndex,
+  BprChristaJayaInformation,
+  BprChristaJayaLogin,
+  DiabseninAnnouncement,
+  DiabseninAnnouncementDetail,
+  DiabseninLogin,
+  DiabseninMaps,
+  DiabseninNotification,
+  DiabseninSignin,
+  BigDataUMKMCatalog,
+  BigDataUMKMIndex,
+  BigDataUMKMDashboard,
+  BigDataUMKMLogin,
+  PortalSmartcoopCoperationData,
+  PortalSmartcoopDashboardStatistik,
+  PortalSmartcoopLogin,
+  PortalSmartcoopStatistik,
+  SekiHalselDashboard,
+  SekiHalselLogin,
+  SilatoKopiSahaDashboard,
+  SilatoKopiSahaIndex,
+  SilatoKopiSahaLogin,
+  SilatoKopiSahaTraining,
+  NeqatAnnouncement,
+  NeqatCheckin,
+  NeqatConfig,
+  NeqatMaps,
+  SipajongaPayment,
+  SipajongaStatistik,
+  PmsDetailTask,
+  PmsTask,
+};
