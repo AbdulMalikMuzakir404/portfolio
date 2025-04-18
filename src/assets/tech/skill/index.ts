101eea5e417b97907c4fb893eb29ba119e1a0d3f
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-04-18 18:16:02 +0700

    Add SVG icons for various technologies: React, Redis, Tailwind, TypeScript, and Vue
    - Added reactjs.svg for React.js technology.
    - Added redis.svg for Redis technology.
    - Added tailwind.svg for Tailwind CSS framework.
    - Added typescript.svg for TypeScript language.
    - Added vue.svg for Vue.js framework.

---
+++ src/assets/tech/skill/index.ts
@@ -0,0 +1,53 @@
+import bootstrapskill from "./bootstrap.svg";
+import cssskill from "./css.svg";
+import dartskill from "./dart.svg";
+import digitaloceanskill from "./digitalocean.svg";
+import dockerskill from "./docker.svg";
+import figmaskill from "./figma.svg";
+import firebaseskill from "./firebase.svg";
+import flutterskill from "./flutter.svg";
+import gitskill from "./git.svg";
+import htmlskill from "./html.svg";
+import javascriptskill from "./javascript.svg";
+import jqueryskill from "./jquery.svg";
+import laravelskill from "./laravel.svg";
+import mysqlskill from "./mysql.svg";
+import nestjsskill from "./nestjs.svg";
+import nextjsskill from "./nextjs.svg";
+import nodejsskill from "./nodejs.svg";
+import phpskill from "./php.svg";
+import prismaskill from "./prisma.svg";
+import psqlskill from "./psql.svg";
+import reactjsskill from "./reactjs.svg";
+import redisskill from "./redis.svg";
+import tailwindskill from "./tailwind.svg";
+import typescriptskill from "./typescript.svg";
+import vueskill from "./vue.svg";
+
+export {
+  bootstrapskill,
+  cssskill,
+  dartskill,
+  digitaloceanskill,
+  dockerskill,
+  figmaskill,
+  firebaseskill,
+  flutterskill,
+  gitskill,
+  htmlskill,
+  javascriptskill,
+  jqueryskill,
+  laravelskill,
+  mysqlskill,
+  nestjsskill,
+  nextjsskill,
+  nodejsskill,
+  phpskill,
+  prismaskill,
+  psqlskill,
+  reactjsskill,
+  redisskill,
+  tailwindskill,
+  typescriptskill,
+  vueskill,
+};
