87b8f6b28ec53bfa263b3938e787d6bc4dc4bcb1
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-04-18 18:15:37 +0700

    feat: add new technology assets and update index for improved resource management

---
+++ src/assets/tech/index.ts
@@ -0,0 +1,55 @@
+import bootstrap from "./bootstrap.png";
+import css from "./css.png";
+import dart from "./dart.png";
+import digitalocean from "./digitalocean.png";
+import docker from "./docker.png";
+import figma from "./figma.png";
+import firebase from "./firebase.png";
+import flutter from "./flutter.png";
+import git from "./git.png";
+import html from "./html.png";
+import javascript from "./javascript.png";
+import jquery from "./jquery.png";
+import laravel from "./laravel.png";
+import livewire from "./livewire.png";
+import mysql from "./mysql.png";
+import nestjs from "./nestjs.png";
+import nextjs from "./nextjs.png";
+import nodejs from "./nodejs.png";
+import php from "./php.png";
+import prisma from "./prisma.png";
+import psql from "./psql.png";
+import reactjs from "./reactjs.png";
+import redis from "./redis.png";
+import tailwind from "./tailwind.png";
+import typescript from "./typescript.png";
+import vue from "./vue.png";
+
+export {
+  bootstrap,
+  css,
+  dart,
+  digitalocean,
+  docker,
+  figma,
+  firebase,
+  flutter,
+  git,
+  html,
+  javascript,
+  jquery,
+  laravel,
+  livewire,
+  mysql,
+  nestjs,
+  nextjs,
+  nodejs,
+  php,
+  prisma,
+  psql,
+  reactjs,
+  redis,
+  tailwind,
+  typescript,
+  vue,
+};
