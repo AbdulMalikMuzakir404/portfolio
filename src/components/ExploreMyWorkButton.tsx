d6a74bd209de03318041b280aca14c8390aae0cb
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-04-19 00:08:32 +0700

    refactor: add BackgroundGradient component and wrap ExploreMyWorkButton for enhanced styling

--- src/components/ExploreMyWorkButton.tsx
+++ src/components/ExploreMyWorkButton.tsx
@@ -1,18 +1,20 @@
 "use client";
-
 import ArrowDown from "@/assets/icons/arrow-down.svg";
+import { BackgroundGradient } from "./ui/BackgroundGradient";
 
 export const ExploreMyWorkButton = () => {
   return (
-    <button
-      onClick={() => {
-        const el = document.getElementById("skills");
-        el?.scrollIntoView({ behavior: "smooth" });
-      }}
-      className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl"
-    >
-      <span className="font-semibold">Explore My Work</span>
-      <ArrowDown className="size-4" />
-    </button>
+    <BackgroundGradient rounded="rounded-xl">
+      <button
+        onClick={() => {
+          const el = document.getElementById("skills");
+          el?.scrollIntoView({ behavior: "smooth" });
+        }}
+        className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl"
+      >
+        <span className="font-semibold">Explore My Work</span>
+        <ArrowDown className="size-4" />
+      </button>
+    </BackgroundGradient>
   );
 };
