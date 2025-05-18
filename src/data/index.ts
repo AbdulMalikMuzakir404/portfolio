151ab188ced8a91a5865a131a520fd4129f7032e
TheDoctor <abdulmalikmuzakir55@gmail.com>
2025-05-18 13:48:00 +0700

    Adjusts grid item height for responsiveness
    Updates the grid item height to use `clamp` for better responsiveness across different screen sizes. This ensures a more consistent and visually appealing layout on various devices.

--- src/data/index.ts
+++ src/data/index.ts
@@ -8,9 +8,10 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "I prioritize client collaboration, fostering open communication",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    className:
+      "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[clamp(40rem, 60vh, 70rem)]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
     img: "/b1.svg",
